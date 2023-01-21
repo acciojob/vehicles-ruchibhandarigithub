e358658ff6e48e2ac063710e08ee8855fc7b5814
ruchibhandarigithub <bhandariruchi498@gmail.com>
2023-01-21 03:28:22 -0800

    message

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,6 +1,6 @@
 package com.driver;
 
-public class Boat implements WaterVehicle{
+ public class Boat implements WaterVehicle{
     private String name ;
     private  int  capacity;
     Boat(String name ,int capacity){
